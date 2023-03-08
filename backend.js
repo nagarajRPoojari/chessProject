2c44e130915f2b7a699cc12c263568cdd97b6315
nagarajRPoojari <np137270@gmail.com>
2023-03-08 19:29:42 +0530

    added databse url

--- backend.js
+++ backend.js
@@ -33,11 +33,11 @@ mongoose.set("strictQuery", false);
 
 const MongoStore = require("connect-mongo")(session);
 
-const dbUrl = "mongodb://localhost:27017/blogdb";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/blogdb";
 //"mongodb://localhost:27017/blogdb"
 //process.env.DB_URL
 mongoose
-  .connect("mongodb://localhost:27017/blogdb", {
+  .connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
